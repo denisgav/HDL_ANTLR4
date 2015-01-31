57d3ce8bcc7ac43a674aae360d442b25806edd6e
Gennadiy Zaychenko <zaychenko.gennadiy@gmail.com>
2015-01-31 19:55:32 +0200

    changed build.cmd

--- build.cmd
+++ build.cmd
@@ -8,11 +8,11 @@ if not defined VS100COMNTOOLS (
     if not defined VS120COMNTOOLS (
         if not defined VS140COMNTOOLS (
             echo "Error: build.cmd should be run from a Visual Studio (2010 or above) Command Prompt."
-            echo "It's located in Microsoft Visual Studio XX.X\VC\vcvarsall.bat"
             exit /b 1
-        )
-    )
-)
+        ) else set TOOLS_DIR=%VS140COMNTOOLS%
+    ) else set TOOLS_DIR=%VS120COMNTOOLS%
+) else set TOOLS_DIR=%VS100COMNTOOLS%
+call "%TOOLS_DIR%\vsvars32.bat"
 
 :: Enforce package restore to avoid build issues. See http://go.microsoft.com/fwlink/?LinkID=317568 for more details
 tools\NuGet.exe restore %SOLUTION_NAME%
