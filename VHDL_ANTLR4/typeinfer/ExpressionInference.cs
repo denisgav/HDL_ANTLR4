ca5a005a3eb390500471600cbf41fea28ba94fa8
Gennadiy Zaychenko <zaychenko.gennadiy@gmail.com>
2015-02-01 19:09:53 +0200

    added NameInference

--- VHDL_ANTLR4/typeinfer/ExpressionInference.cs
+++ VHDL_ANTLR4/typeinfer/ExpressionInference.cs
@@ -10,7 +10,7 @@ namespace VHDL.parser.typeinfer
     {
         private TypeInference baseInfer;
         private IDeclarativeRegion scope;
-        public IFunction  convertFunction;
+        public IFunction convertFunction;
 
         public ExpressionInference(IDeclarativeRegion scope, TypeInference baseInfer)
         {
@@ -18,8 +18,10 @@ namespace VHDL.parser.typeinfer
             this.scope = scope;
         }
 
-        public IFunction  ConvertFunction
-        {get{return convertFunction;}}
+        public IFunction ConvertFunction
+        {
+            get { return convertFunction; }
+        }
 
         protected override void visitLiteral(Literal expression)
         {
@@ -29,34 +31,8 @@ namespace VHDL.parser.typeinfer
 
         protected override void visitName(Name name)
         {
-            var arrElem = name as ArrayElement;
-            if (arrElem != null)
-            {
-            }
-
-            var recElem = name as RecordElement;
-            if (recElem != null)
-            {
-                if (CheckTypeRelation(baseInfer.ExpectedType, recElem.Type))
-                    baseInfer.ResultType = recElem.Type;
-            }
-
-            var attribute = name as AttributeExpression;
-            if (attribute != null)
-            {
-                if (CheckTypeRelation(baseInfer.ExpectedType, attribute.Type))
-                    baseInfer.ResultType = attribute.Type;
-            }
-        }
-
-        public bool CheckTypeRelation(ISubtypeIndication expected, ISubtypeIndication actual)
-        {
-            string expectedName = TypeHelper.GetTypeName(expected);
-            string actual_name = TypeHelper.GetTypeName(actual);
-            if (expectedName != "" && expectedName == actual_name)
-                return true;
-
-            return false;
+            var nameInfer = new NameInference(scope, baseInfer);
+            name.accept(nameInfer);
         }
     }
 }
---
+++ VHDL_ANTLR4/typeinfer/NameInference.cs
@@ -0,0 +1,36 @@
+﻿using VHDL;
+using VHDL.expression;
+using VHDL.Object;
+
+namespace VHDL.parser.typeinfer
+{
+    class NameInference : NameVisitor
+    {
+        private TypeInference inferer;
+        private IDeclarativeRegion scope;
+
+        public NameInference(IDeclarativeRegion scope, TypeInference baseInfer)
+        {
+            this.inferer = baseInfer;
+            this.scope = scope;
+        }
+
+        public override void visit(RecordElement name)
+        {
+            if (TypeInference.AreTypesEqual(inferer.ExpectedType, name.Type))
+                inferer.ResultType = name.Type;
+        }
+
+        public override void visit(AttributeExpression name)
+        {
+            if (TypeInference.AreTypesEqual(inferer.ExpectedType, name.Type))
+                inferer.ResultType = name.Type;
+        }
+
+        public override void visit(VhdlObject name)
+        {
+            if (TypeInference.AreTypesCompatible(inferer.ExpectedType, name.Type))
+                inferer.ResultType = name.Type;
+        }
+    }
+}
